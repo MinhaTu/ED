31e102050616516e3561a93347b7a20462daafd4
Ailson <uchiha.ailson@hotmail.com>
2017-11-16 15:25:56 -0200

    Atualizando

--- Listas/Deque/deque.cpp
+++ Listas/Deque/deque.cpp
@@ -17,9 +17,41 @@ void inicializar_deque(Deque<T> &D){
 	D.ini = D.fim = NULL;
 }
 template <typename T>
-bool inserir_esq(Deque<T> &D);
+bool inserir_esq(Deque<T> &D, T e){
+	NohD<T> *n = new (nothrow) NohD<T>;
+	if(n){
+		n->elem = e;
+		n->ant = NULL;
+		n->prox = D.ini;
+		if(D.ini == NULL){
+			D.fim = n;
+		}else{
+			D.ini->ant = n;
+		}
+		D.ini = n;
+		return 1; // sem erro
+	}else{
+		return 0;// com erro
+	}
+}
+template <typename T>
+T remover_esq(Deque<T> &D){
+	T e = D.ini->elem;
+	if(D.ini == D.fim){
+		D.ini = D.fim = NULL;
+		return e;
+	}
+	D.ini = D.ini->prox;
+	delete D.ini->ant;
+	return e;
+}
+
 int main(int argc, char const *argv[])
-{
-	
+{	Deque<int> D;
+	int n;
+	inicializar_deque(D);
+	inserir_esq(D,4);
+	inserir_esq(D,2);
+	cout << remover_esq(D) << " ";
 	return 0;
 }
