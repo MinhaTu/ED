61aad632bc5f906938811dd9a3a61cf5f64ea034
MinhaTu <ailson.alexandre@hotmail.com>
2017-11-18 20:28:29 -0300

    Fixing sintaxe problems

--- Listas/Deque/deque.cpp
+++ Listas/Deque/deque.cpp
@@ -40,9 +40,9 @@ bool inserir_dir(Deque<T> &D, T e){
 	if(n){
 		n->elem = e;
 		n->prox = NULL;
-		n<-ant = D.fim;
+		n->ant = D.fim;
 		if(D.fim == NULL){
-			D.ini == NULL;
+			D.ini == n;
 		}else{
 			D.fim->prox = n;
 		}
@@ -55,28 +55,32 @@ bool inserir_dir(Deque<T> &D, T e){
 template <typename T>
 T remover_esq(Deque<T> &D){
 	T e = D.ini->elem;
+	NohD<T> *n = D.ini;
 	if(D.ini == D.fim){
 		D.ini = D.fim = NULL;
-		return e;
-	}
-	D.ini = D.ini->prox;
-	delete D.ini->ant;
-	return e;
-}
-template <typename T>
-T remover_esq(Deque<T> &D){
-	T e = D.fim->elem;
-	if(D.fim == D.fim){
-		D.fim = D.ini = NULL;
 	}else{
 		D.ini = D.ini->prox;
 	}
-	delete D.ini->ant;
+	delete n;
 	return e;
 }
+template <typename T>
+T remover_dir(Deque<T> &D){
+	T e = D.fim->elem; 
+	NohD<T> *n = D.fim;
+	if(D.ini == D.fim){
+		D.fim = D.ini = NULL;
+	}else{
+		D.fim = D.fim->ant;
+	}
+	delete n;
+	return e;
+}
+template <typename T>
 bool vazia(Deque<T> &D){
 	return !(D.fim=NULL);
 }
+template <typename T>
 void terminar_deque(Deque<T> &D){
 	D.fim = NULL;
 	while(D.ini != D.fim){
@@ -88,7 +92,8 @@ int main(int argc, char const *argv[])
 {	Deque<int> D;
 	int n;
 	inicializar_deque(D);
+	inserir_esq(D,5);
 	inserir_esq(D,2);
-	cout << remover_esq(D) << " ";
+	cout  <<  remover_dir(D) << " " << remover_dir(D);
 	return 0;
 }
